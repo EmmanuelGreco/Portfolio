3ed8a5c1dcdfd5903e1fc5643dec74d3752fa3fe
Emmanuel Greco <emma.greco.96@gmail.com>
2025-09-18 18:34:51 -0300

    Estructura y Funcionalidad del Home. Completo Header

---
+++ Portfolio/static/js/script.js
@@ -0,0 +1,7 @@
+const typed = new Typed('.animacion-cargo', {
+    strings: ['Back-End Developer Trainee', 'Web Developer Trainee'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 2000,
+    loop: true
+});
