c948b599793cb84902a3d5619914c01a643bf2c8
Emmanuel Greco <emma.greco.96@gmail.com>
2025-09-18 23:08:30 -0300

    Estructura y Funcionalidad del Sobre Mi

--- Portfolio/static/js/script.js
+++ Portfolio/static/js/script.js
@@ -1,7 +1,30 @@
 const typed = new Typed('.animacion-cargo', {
-    strings: ['Back-End Developer Trainee', 'Web Developer Trainee'],
+    strings: ['^1000Back-End Developer Trainee', '^1000Web Developer Trainee'],
     typeSpeed: 50,
-    backSpeed: 50,
+    backSpeed: 30,
     backDelay: 2000,
     loop: true
+});
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".navbar a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach(section => {
+    // Le resto 100px, para que se considera activa, ya que la navbar tapa parte de las sección anterior.
+    const sectionTop = section.offsetTop - 100;
+    const sectionHeight = section.clientHeight;
+    if (scrollY >= sectionTop && scrollY < sectionTop + sectionHeight) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === "#" + current) {
+      link.classList.add("active");
+    }
+  });
 });
